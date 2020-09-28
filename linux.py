70cfe03f72a59eea51e3604993ff2e6a94aed3d5
Rahul Meena <52858429+404notfound-3@users.noreply.github.com>
2020-09-28 21:40:54 +0530

    improved readability

--- linux.py
+++ linux.py
@@ -1,13 +1,22 @@
-from pytz import timezone
+import csv
+import time
+import json
+import random
+import pickle
+from sys import exit
 from stem import Signal
-from stem.control import Controller
-from slack_webhook import Slack
+from requests import get
+from pytz import timezone
+from os import popen, path
 from datetime import datetime
 from selenium import webdriver
+from slack_webhook import Slack
+from traceback import format_exc
 from bs4 import BeautifulSoup as bs
+from stem.control import Controller
 from pyvirtualdisplay import Display
-import json, csv, os, time, random, sys, traceback, pickle, requests
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
 
 URL = "https://instagram.com/{}/" ## Instagram URL
 SOCKSPort = 9050 ## Tor SOCKS listner port
@@ -34,9 +43,14 @@ proxies = {
 
 ## to verify Tor connectivity
 def check_tor_conn():
-    title = str(bs(requests.get("https://check.torproject.org", proxies = proxies).text, "html.parser").title).split("\n")[2]
-    if "Congratulations." in title: print("\nTor Connectivity Verified"), slack.post(text = "\nTor Connectivity Verified")
-    elif "not using Tor" in title: print("\nTor is not Connected, Try 'sudo service tor restart'. Aborting..."), slack.post(text = "\nTor is not Connected, Try 'sudo service tor restart'. Aborting..."), sys.exit(0)
+    title = str(bs(get("https://check.torproject.org", proxies = proxies).text, "html.parser").title).split("\n")[2]
+    if "Congratulations." in title:
+        print("\nTor Connectivity Verified")
+        slack.post(text = "\nTor Connectivity Verified")
+    elif "not using Tor" in title:
+        print("\nTor is not Connected, Try 'sudo service tor restart'. Aborting...")
+        slack.post(text = "\nTor is not Connected, Try 'sudo service tor restart'. Aborting...")
+        exit(0)
 check_tor_conn()
 
 ## To create virtual display.
@@ -111,15 +125,16 @@ def parse_data(page_source_soup):
 def scrape_data(username):
     browser.get(URL.format(username))
     page_source = browser.page_source
-    return parse_data(bs(page_source, "html.parser"))
+    return parse_data(bs(browser.page_source, "html.parser"))
 
 ## This function will take data dictonary in input and will save all of data "smartly" in a csv file. 
 def check_csv_file(data, filename):
     ## If the csv file exist or you're this code again this will check for your existing data in 
-    if os.path.isfile(OUTPUT_FOLDER + filename):
+    if path.isfile(OUTPUT_FOLDER + filename):
         with open(OUTPUT_FOLDER + filename, "r") as rf:
             last_line_items, true_false_list, specific_key_index = rf.readlines()[-1].split(","), [], [0, 2, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-            for key_index in specific_key_index: true_false_list.append(data[list(data.keys())[key_index]] == last_line_items[key_index])
+            for key_index in specific_key_index:
+                true_false_list.append(data[list(data.keys())[key_index]] == last_line_items[key_index])
             if False in true_false_list:
                 with open(OUTPUT_FOLDER + filename, "a", newline = "") as csvfile:
                     csvwriter = csv.writer(csvfile)
@@ -129,34 +144,51 @@ def check_csv_file(data, filename):
     else:
         with open(OUTPUT_FOLDER + filename, "a", newline = "") as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(list(data.keys())), csvwriter.writerow([data[keys] for keys in data.keys()])
+            csvwriter.writerow(list(data.keys()))
+            csvwriter.writerow([data[keys] for keys in data.keys()])
 
 ## this function will help us to create randomness in scraper, so we can save our little a** from facebook's anti scraping tools.
-def random_sleep_time(secends): return random.randint(secends - (secends * 0.3), secends + (secends * 0.4))
+def random_sleep_time(secends):
+    return random.randint(secends - (secends * 0.3), secends + (secends * 0.4))
 
 ## Exit options will called if you pressed "CTRL+C"
 def exit_options():
-    print("\n"), print("\nWelcome to exit options, you have choices below\n1). Exit and close this script\n\n2). Do nothing, I intrupted accidently")
+    print("\n\nWelcome to exit options, you have choices below\n1). Exit and close this script\n\n2). Do nothing, I intrupted accidently")
     your_choice = int(input("\nYour command >>> "))
-    if your_choice == 1: sys.exit(0)
+    if your_choice == 1:
+        exit()
     else: pass
 
 ## This function will take instagram credentials as input, and will login your id as output in browser.
 ## Warning please make sure that you are using temporary ids to signin and scraping profiles.
 def ig_login(email, password):
-    print(f"\nTrying to logging in using {email}..."), slack.post(text = f"Trying to logging in using {email}"), browser.get("https://instagram.com"), browser.execute_script("window.scrollTo(0,document.body.scrollHeight)"), time.sleep(2.6), browser.execute_script("window.scrollTo(document.body.scrollHeight, 0)"), time.sleep(0.6)
+    print(f"\nTrying to logging in using {email}...")
+    slack.post(text = f"Trying to logging in using {email}")
+    browser.get("https://instagram.com")
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(2.6)
+    browser.execute_script("window.scrollTo(document.body.scrollHeight, 0)")
+    time.sleep(0.6)
     a = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
-    a.click(), time.sleep(3.7)
-    for words in email: a.send_keys(words), time.sleep(0.3)
+    a.click()
+    time.sleep(3.7)
+    for words in email: a.send_keys(words)
+    time.sleep(0.3)
     b = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
-    b.click(), time.sleep(1.6)
-    for words in password: b.send_keys(words), time.sleep(0.25)
-    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click(), print(f"\nSuccessfully logged in {email}..."), slack.post(text = f"Successfully logged in {email}"), time.sleep(20)
-    try: browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click(), time.sleep(15), browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click(), time.sleep(15)
-    except: pass
-
-## i'm currently working on this function, maybe you'll see it in upcoming commits/versions.
-def specific_user_aleart(data): pass
+    b.click()
+    time.sleep(1.6)
+    for words in password: b.send_keys(words)
+    time.sleep(0.25)
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+    print(f"\nSuccessfully logged in {email}...")
+    slack.post(text = f"Successfully logged in {email}")
+    time.sleep(20)
+    try:
+        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+        time.sleep(15)
+        browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(15)
+    except exception as e: pass ## Needs to be solved
 
 ## request to our browser to take and save an screenshot "smartly".
 t1 = None
@@ -165,7 +197,8 @@ def save_ss(event = None):
     t2 = time.time()
     if not t1 or ((t2-t1)>120):# 2Minutes
         now = datetime.now(timezone(TimeZone)) ## Change timezone according to your country or state.
-        browser.get_screenshot_as_file(f'./ss_log/browser/{now.strftime("%d_%m_%Y")}__{now.strftime("%H_%M")}.png'), print(f'\nBrowser Screenshot saved at ./ss_log/browser/{now.strftime("%d_%m_%Y")}__{now.strftime("%H_%M")}.png')
+        browser.get_screenshot_as_file(f'./ss_log/browser/{now.strftime("%d_%m_%Y")}__{now.strftime("%H_%M")}.png')
+        print(f'\nBrowser Screenshot saved at ./ss_log/browser/{now.strftime("%d_%m_%Y")}__{now.strftime("%H_%M")}.png')
         t1 = time.time() ## t1 will be replaced with current time if we succeed to take a screenshot.
     else: print(f"\nScreenshot events cancelled because last ss was taken {t2-t1} secends ago")
 
@@ -182,20 +215,31 @@ def slack_hook(username, exception):
 
 ## To get public IP of Tor
 def get_tor_ip():
-    try: return requests.get("http://httpbin.org/ip", proxies = proxies).text.split('"')[3] ## return ip as string
+    try:
+        return get("http://httpbin.org/ip", proxies = proxies).text.split('"')[3] ## return ip as string
     except Exception as e:
-        exception = traceback.format_exc()
+        exception = format_exc()
         print(f"\nException Occurred while checking current ip\n", str(exception))
 
 ## To change Tor identity(IP)
 def renew_tor_ip():
     ## change ControlPort as your configuration.
     with Controller.from_port(port = ControlPort) as controller:
-        controller.authenticate(), controller.signal(Signal.NEWNYM) ## input authentication password as string.
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM) ## input authentication password as string.
 
 
 ## This Function will log out your id from browser, after logout succeed all cookies will be deleted so we can login new id.
-def ig_logout(email): browser.get("https://instagram.com"), print(f"\nTrying to logging out {email}..."), slack.post(text = f"Trying to logging out {email}"), browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span').click(), time.sleep(1), browser.find_element_by_xpath('//div[text() = "Log Out"]').click(), browser.delete_all_cookies(), print(f"\nSuccessfully logged out {email}"), slack.post(text = f"Successfully logged out {email}")
+def ig_logout(email):
+    browser.get("https://instagram.com")
+    print(f"\nTrying to logging out {email}...")
+    slack.post(text = f"Trying to logging out {email}")
+    browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span').click()
+    time.sleep(1)
+    browser.find_element_by_xpath('//div[text() = "Log Out"]').click()
+    browser.delete_all_cookies()
+    print(f"\nSuccessfully logged out {email}")
+    slack.post(text = f"Successfully logged out {email}")
 
 temp_id1 = random.choice([keys for keys in ids.keys()]) ## To load credentials from ids dictionary (randomly).
 ig_login(email = temp_id1, password = ids[temp_id1]) ## To login your temprary id in browser
@@ -206,9 +250,11 @@ while True: ## To keep running this scraper.
         filename = f"{username}.csv" ## name of csv file
         try:
             data = scrape_data(username)
-            check_csv_file(data, filename), print("\n", username, " ", data)
+            check_csv_file(data, filename)
+            print("\n", username, " ", data)
             sleep_time = random_sleep_time(120)# 2 Minutes
-            print(f"\nsleeping for {sleep_time} secends....."), time.sleep(sleep_time)
+            print(f"\nsleeping for {sleep_time} secends.....")
+            time.sleep(sleep_time)
             
             ## To logout automaticly if script is running from past 8 hours, else instagram will block our temprory id.
             if (time.time()-t5) > (8*3600):# 8 Hours
@@ -216,7 +262,11 @@ while True: ## To keep running this scraper.
                 t5, temp_id2 = time.time(), random.choice([keys for keys in ids.keys()])
                 ig_login(email = temp_id2, password = ids[temp_id2]) ## login new id
                 temp_id1 = temp_id2 ## variable will replaed if autologin succeed.
-        except KeyboardInterrupt: print("\n^C Detected. Taking you to exit options...."), exit_options()
+        except KeyboardInterrupt:
+            print("\n^C Detected. Taking you to exit options....")
+            exit_options()
         except Exception as e:
-            exception = traceback.format_exc() ## to format exceptions
-            print(f"\nException Occurred while scraping for {username}", str(exception)), save_ss(), slack_hook(username = username, exception = exception)
+            exception = format_exc() ## to format exceptions
+            print(f"\nException Occurred while scraping for {username}", str(exception))
+            save_ss()
+            slack_hook(username, exception)
